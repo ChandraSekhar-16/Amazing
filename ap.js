8ad6f7d9253afaf50847b1b2cdcfeae7a9c3fded
Chandra Sekhar <shekharchandra1606@gmail.com>
2026-05-11 09:09:43 +0530

    Initial Commiy

--- ap.js
+++ ap.js
@@ -2,7 +2,6 @@ const video1 = document.getElementById('projectVideo1');
 const video2 = document.getElementById('projectVideo2');
 const video3 = document.getElementById('projectVideo3');
 
-// Sidebar elements //
 const sideBar = document.querySelector('.sidebar');
 const menu = document.querySelector('.menu-icon');
 const closeIcon = document.querySelector('.close-icon')
@@ -23,7 +22,6 @@ videoList.forEach (function(video){
 })
 })
 
-// Sidebar elements //
 menu.addEventListener("click", function(){
     sideBar.classList.remove("close-sidebar")
     sideBar.classList.add("open-sidebar")
